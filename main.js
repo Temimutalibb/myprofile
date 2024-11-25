62b3b4d7999d4e91972f44337b28dedb74fe93e9
Mutalibb <temitopking@icloud.com>
2024-11-25 10:25:27 -0700

    jquery and sass used

--- main.js
+++ main.js
@@ -1,71 +1,167 @@
-import './style.css';
-const menuButton = document.getElementById('header-dropdown');
-const themeButton = document.querySelector('.theme');
-const dropdown = document.querySelector('#dropdown');
-const close = document.querySelector('.close');
-const themeSelect = document.querySelector('.themeselect');
-const stackRow  = document.getElementById('stack');
-const arrowBack = document.querySelector('.arrowleft');
-const arrowForward = document.querySelector('.arrowright');
-const themeRadio =  document.getElementsByName('theme');
-const bodyColor = document.querySelector('.bodycolor');
+import $ from "jquery";
+import "./style.css";
+import "./styles.scss";
 
-menuButton.addEventListener("click", (event) =>{
-    event.stopPropagation()
-    dropdown.style.display = "block";
-    close.addEventListener("click",()=>{
-        dropdown.style.display = "none"
+//for the option styling
+$(".option").on("click", function () {
+  $(".option").removeClass("active");
+  $(this).addClass("active");
+});
+
+//custom boostrap style for project modal
+$("#demostorebtn").on("click", function () {
+  const myModal = new bootstrap.Modal(document.getElementById("demostore"));
+  myModal.show();
+});
+$("#profilebtn").on("click", function () {
+  const myModal = new bootstrap.Modal(document.getElementById("portfolio"));
+  myModal.show();
+});
+$("#kingsbtn").on("click", function () {
+  const myModal = new bootstrap.Modal(document.getElementById("kingsclean"));
+  myModal.show();
+});
+$("#wiibtn").on("click", function () {
+  const myModal = new bootstrap.Modal(document.getElementById("wiiapp"));
+  myModal.show();
+});
+$("#rokonotebtn").on("click", function () {
+  const myModal = new bootstrap.Modal(document.getElementById("rokonote"));
+  myModal.show();
+});
+//////////////////////////
+
+//for tools styling
+$(".card_card").each(function () {
+  let $pointer = $(this).find(".pointer");
+  $(this).on("mousemove", function (event) {
+    let pointerX = event.pageX - $(this).offset().left - $pointer.width() / 2;
+    let pointerY = event.pageY - $(this).offset().top - $pointer.height() / 2;
+    $pointer.css({
+      left: pointerX + "px",
+      top: pointerY + "px",
+      backgroundImage: `hsl(${$(this).css("--hue")}, ${$(this).css(
+        "--saturation"
+      )}, ${$(this).css("--lightness")})`,
     });
-})
+    let boxWidth = $(this).width();
+    let boxHeight = $(this).height();
+    let gradientX = (pointerX / boxWidth) * 100;
+    let gradientY = (pointerY / boxHeight) * 100;
+    let hue = parseFloat($(this).css("--hue"));
+    let saturation = parseFloat($(this).css("--saturation"));
+    let lightness = parseFloat($(this).css("--lightness"));
 
-themeButton.addEventListener("click",(event) =>{
-    event.stopPropagation()
-    if(themeSelect.style.display === "block"){
-        themeSelect.style.display="none"
-    }else{
-        themeSelect.style.display="block"
+    $(this).find(".border-top").css({ display: "block" });
+    $(this).find(".border-bottom").css({ display: "block" });
+    $(this).find(".pointer").css({
+      display: "block",
+    });
+    $(this).css(
+      "backgroundImage",
+      `linear-gradient(45deg, hsl(${hue}, ${saturation}%, 50%), hsl(${
+        (hue + gradientX) % 360
+      }, ${saturation}%, ${lightness + (gradientY / 100) * 50}%))`
+    );
+    let borderColor = $(this).css("--border-color");
+    if (pointerX <= boxWidth / 2) {
+      $(this).css({
+        borderLeftColor: borderColor,
+        borderRightColor: "transparent",
+      });
+    } else {
+      $(this).css({
+        borderLeftColor: "transparent",
+        borderRightColor: borderColor,
+      });
     }
-})
+  });
 
-stackRow.addEventListener("mouseover",() =>{
- if(arrowBack.style.display === "block"){
-     arrowBack.style.display ="none"
-      arrowForward.style.display ="none"
- }else{
-      arrowBack.style.display ="block"
-      arrowForward.style.display ="block"
-    }
-})
+  $(this).on("mouseleave", function () {
+    $(this).css({
+      borderColor: "transparent",
+      border: "transparent",
+      backgroundImage: "none",
+    });
+    $(this)
+      .find(".border-top, .border-bottom, .pointer")
+      .css({ display: "none" });
+  });
+});
 
+$(".card_card").on("mousemove", function (event) {
+  let pointerX = event.pageX - $(this).offset().left;
+  let width = $(this).width();
+  $(this)
+    .find(".border-top")
+    .css({ left: (pointerX / width) * 75 + "%" });
+  $(this)
+    .find(".border-bottom")
+    .css({ left: (pointerX / width) * 75 + "%" });
+});
+////////////////////////////////////////////////
 
-themeRadio.forEach((element) =>{
-    element.addEventListener(
-        "change", (e) =>{
-            let themeValue;
-            themeValue = e.target.value
-            //dropdown.classList = `dropdown ${themeValue}`
-            bodyColor.classList= themeValue;
-        }
-    )
-})
+///vanilla for headers theme and drop down
+const themeButton = document.querySelector(".theme");
+const themeSelect = document.querySelector(".themeselect");
+const themeRadio = document.getElementsByName("theme");
+const bodyColor = document.querySelector(".bodycolor");
+const menuButton = document.getElementById("header-dropdown");
+const dropdown = document.querySelector("#dropdown");
+const close = document.querySelector(".close");
+const stackRow = document.getElementById("stack");
+const arrowBack = document.querySelector(".arrowleft");
+const arrowForward = document.querySelector(".arrowright");
 
+themeButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  if (themeSelect.style.display === "block") {
+    themeSelect.style.display = "none";
+  } else {
+    themeSelect.style.display = "block";
+  }
+});
 
-document.addEventListener("click",() =>{
-    if (themeSelect.style.display === 'block'){
-        themeSelect.style.display = 'none';
-    }
-    
-})
+themeRadio.forEach((element) => {
+  element.addEventListener("change", (e) => {
+    let themeValue;
+    themeValue = e.target.value;
+    //dropdown.classList = `dropdown ${themeValue}`
+    bodyColor.classList = themeValue;
+  });
+});
 
+menuButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  dropdown.style.display = "block";
+  close.addEventListener("click", () => {
+    dropdown.style.display = "none";
+  });
+});
 
-//to capture the screen size
-window.addEventListener('resize', () => {
-    const screen = window.innerWidth
-     if(screen >= 1000){
-      dropdown.style.display = 'block'
-     }
-     
-     if(screen < 1000){
-          dropdown.style.display = ''
-     }
-})
+stackRow.addEventListener("mouseover", () => {
+  if (arrowBack.style.display === "block") {
+    arrowBack.style.display = "none";
+    arrowForward.style.display = "none";
+  } else {
+    arrowBack.style.display = "block";
+    arrowForward.style.display = "block";
+  }
+});
+
+window.addEventListener("resize", () => {
+  const screen = window.innerWidth;
+  if (screen >= 1000) {
+    dropdown.style.display = "block";
+  }
+
+  if (screen < 1000) {
+    dropdown.style.display = "";
+  }
+});
+
+document.addEventListener("click", () => {
+  if (themeSelect.style.display === "block") {
+    themeSelect.style.display = "none";
+  }
+});
---
+++ modal.js
@@ -0,0 +1,381 @@
+const modal = document.querySelector(".bootstrapmodal");
+modal.innerHTML = `
+<!--for demostore-->
+<div
+      class="modal fade"
+      id="demostore"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">DemoStore</h5>
+            <button
+              type="button"
+              class="btn-close btn-danger"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <div class="card" style="width: 100%">
+              <div class="card-body">
+                <h5 class="card-title">e-commerce</h5>
+                <p class="card-text">
+                  Developed a clothing store web app. I made the UI interactive
+                  and provided a good user experience. Users can view a lot of
+                  options without having to navigate through multiple routes.
+                  The colors are well-chosen, complementing the beautiful
+                  clothing and making the site vibrant and colorful.
+                </p>
+
+                <p class="lead">Tech Stack: React, css.</p>
+                <p>
+                  <img
+                    src="/img/demostore.jpeg"
+                    alt="demo store"
+                    class="card-img-top"
+                    alt="demo store"
+                    alt="cleaning page"
+                  />
+                </p>
+                <p>
+                  <a
+                    href="https://demostore-delta.vercel.app/"
+                    target="_blank"
+                    class="btn btn-secondary"
+                    >Live Link</a
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <a
+              href="https://demostore-delta.vercel.app/"
+              target="_blank"
+              class="btn btn-secondary"
+              >Live Link</a
+            >
+            <button
+              type="button"
+              class="btn btn-danger"
+              data-bs-dismiss="modal"
+            >
+              x
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--for rokonote-->
+
+     <div
+    class="modal fade"
+    id="rokonote"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div
+      class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+    >
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+            Roko Note
+          </h5>
+          <button
+            type="button"
+            class="btn-close btn-danger"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="card" style="width: 100%">
+            <div class="card-body">
+              <h5 class="card-title">utility</h5>
+              <p class="card-text">
+                It's Designed to be both handy and user-friendly, it’s
+                perfect for capturing and organizing your thoughts on
+                the go.<br />
+
+                <b>Ease of Use:</b> Streamlined design for quick
+                note-taking.<br />
+
+                <b>Share Button:</b> Easily share your notes with a
+                single click.<br />
+
+                <b>Integration:</b> Selected sites that reward you for
+                posting and interacting with content.<br />
+              </p>
+
+              <p class="lead">
+                Tech Stack: MongoDb, expressjs, React, nodejs, mui,
+                Redux.
+              </p>
+              <p>
+                <img
+                  src="/img/rokonote.png"
+                  alt="roko note"
+                  class="card-img-top"
+                />
+              </p>
+              <p>
+                <a
+                  href="https://roko-note.vercel.app/"
+                  target="_blank"
+                  class="btn btn-secondary"
+                  >Live Link</a
+                >
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <a
+            href="https://roko-note.vercel.app/"
+            target="_blank"
+            class="btn btn-secondary"
+            >Live Link</a
+          >
+          <button
+            type="button"
+            class="btn btn-danger"
+            data-bs-dismiss="modal"
+          >
+            x
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!--for wiiapp-->
+  <div
+  class="modal fade"
+  id="wiiapp"
+  tabindex="-1"
+  aria-labelledby="exampleModalLabel"
+  aria-hidden="true"
+>
+  <div
+    class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+  >
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Wii App</h5>
+        <button
+          type="button"
+          class="btn-close btn-danger"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="modal-body">
+        <div class="card" style="width: 100%">
+          <div class="card-body">
+            <h5 class="card-title">communication</h5>
+            <p class="card-text">
+              This is a demo project I developed to showcase my
+              Next.js skills. It’s a full-stack messaging app
+              designed for chatting, offering a smooth and
+              user-friendly experience
+            </p>
+
+            <p class="lead">
+              Tech Stack: Nextjs, Tailwind, PostgreSQL, Typescript,
+              vite
+            </p>
+            <p>
+              <img
+                src="/img/wiiapp.png"
+                alt="demo store"
+                class="card-img-top"
+                alt="demo store"
+                alt="cleaning page"
+              />
+            </p>
+            <p>
+              <a
+                href="https://wii-app.vercel.app/"
+                target="_blank"
+                class="btn btn-secondary"
+                >Live Link</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <a
+          href="https://wii-app.vercel.app/"
+          target="_blank"
+          class="btn btn-secondary"
+          >Live Link</a
+        >
+        <button
+          type="button"
+          class="btn btn-danger"
+          data-bs-dismiss="modal"
+        >
+          x
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!----for clean king>
+
+   <div
+   class="modal fade"
+   id="kingsclean"
+   tabindex="-1"
+   aria-labelledby="exampleModalLabel"
+   aria-hidden="true"
+ >
+   <div
+     class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+   >
+     <div class="modal-content">
+       <div class="modal-header">
+         <h5 class="modal-title" id="exampleModalLabel">
+           King's clean
+         </h5>
+         <button
+           type="button"
+           class="btn-close btn-danger"
+           data-bs-dismiss="modal"
+           aria-label="Close"
+         ></button>
+       </div>
+       <div class="modal-body">
+         <div class="card" style="width: 100%">
+           <div class="card-body">
+             <h5 class="card-title">landing page</h5>
+             <p class="card-text">
+               The primary goal of this landing page is to clearly
+               communicate our services to visitors as soon as they
+               arrive. It ensures a seamless user experience with
+               interactive elements and fast loading times. The
+               design is straightforward yet comprehensive, providing
+               all the necessary information efficiently.
+             </p>
+
+             <p class="lead">Tech Stack: HTML | CSS | javaScript</p>
+             <p>
+               <img
+                 src="/img/cleaning.jpeg"
+                 alt="demo store"
+                 class="card-img-top"
+                 alt="demo store"
+                 alt="cleaning page"
+               />
+             </p>
+             <p>
+               <a
+                 href="https://kingsclean.vercel.app/"
+                 target="_blank"
+                 class="btn btn-secondary"
+                 >Live Link</a
+               >
+             </p>
+           </div>
+         </div>
+       </div>
+       <div class="modal-footer">
+         <a
+           href="https://kingsclean.vercel.app/"
+           target="_blank"
+           class="btn btn-secondary"
+           >Live Link</a
+         >
+         <button
+           type="button"
+           class="btn btn-danger"
+           data-bs-dismiss="modal"
+         >
+           x
+         </button>
+       </div>
+     </div>
+   </div>
+ </div>
+  
+ <!--for port folio-->
+
+ 
+ <div
+ class="modal fade"
+ id="portfolio"
+ tabindex="-1"
+ aria-labelledby="exampleModalLabel"
+ aria-hidden="true"
+>
+ <div
+   class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+ >
+   <div class="modal-content">
+     <div class="modal-header">
+       <h5 class="modal-title" id="exampleModalLabel">
+         Portfolio
+       </h5>
+       <button
+         type="button"
+         class="btn-close btn-danger"
+         data-bs-dismiss="modal"
+         aria-label="Close"
+       ></button>
+     </div>
+     <div class="modal-body">
+       <div class="card" style="width: 100%">
+         <div class="card-body">
+           <h5 class="card-title">Portfolio</h5>
+           <p class="card-text">
+             I developed my personal portfolio using vanilla
+             JavaScript to have control over every line of code as
+             much as possible, and styled it perfectly with CSS.
+           </p>
+
+           <p class="lead">
+             Tech Stack: HTML,CSS, Bootstrap, javaScript.
+           </p>
+           <p>
+             <img
+               src="/img/myprofile.png"
+               alt="demo store"
+               class="card-img-top"
+               alt="demo store"
+               alt="cleaning page"
+             />
+           </p>
+           <p>
+             <a href="#" target="_blank" class="btn btn-secondary"
+               >Live Link</a
+             >
+           </p>
+         </div>
+       </div>
+     </div>
+     <div class="modal-footer">
+       <a href="/#" target="_blank" class="btn btn-secondary"
+         >Live Link</a
+       >
+       <button
+         type="button"
+         class="btn btn-danger"
+         data-bs-dismiss="modal"
+       >
+         x
+       </button>
+     </div>
+   </div>
+ </div>
+</div>
+</div>
+`;
